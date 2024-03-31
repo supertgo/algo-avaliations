420c88b3c7a6f6e0e34af11e3e64dc05360e1fc8
supertgo <thiagoroberto2003@gmail.com>
2024-03-31 20:29:35 -0300

    feat: add table

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <chrono>
 #include <cmath>
+#include <iomanip>
 #include <iostream>
 
 unsigned long int recursive_fib_count = 0;
@@ -31,15 +32,15 @@ void call_iterative_fib(int seconds) {
 }
 
 unsigned long long int
-fibonacciWithTimeLimit(int n, std::chrono::steady_clock::time_point end) {
+fibonacci_with_time_limit(int n, std::chrono::steady_clock::time_point end) {
   if (std::chrono::steady_clock::now() >= end) {
     return 0;
   }
 
   if (n <= 1)
     return n;
-  return fibonacciWithTimeLimit(n - 1, end) +
-         fibonacciWithTimeLimit(n - 2, end);
+  return fibonacci_with_time_limit(n - 1, end) +
+         fibonacci_with_time_limit(n - 2, end);
 }
 
 void recursive_fib_with_time_limit(int seconds) {
@@ -50,13 +51,14 @@ void recursive_fib_with_time_limit(int seconds) {
       start + std::chrono::seconds(seconds);
 
   unsigned int n = 1;
-  while (fibonacciWithTimeLimit(n, end) != 0) {
+  while (fibonacci_with_time_limit(n, end) != 0) {
     n++;
     recursive_fib_count++;
   }
 }
 
 int main() {
+  int recursive_data[8], iterative_data[8];
 
   for (int s = 15; s <= 120; s += 15) {
 
@@ -67,8 +69,32 @@ int main() {
 
     std::cout << "Iterativo " << iterative_fib_count << std::endl;
 
+    recursive_data[s / 15 - 1] = recursive_fib_count;
+    iterative_data[s / 15 - 1] = iterative_fib_count;
+
     recursive_fib_count = iterative_fib_count = 0;
   }
+
+  std::cout << std::setw(10) << "Versão" << std::setw(10) << "15s"
+            << std::setw(10) << "30s" << std::setw(10) << "45s" << std::setw(10)
+            << "60s" << std::setw(10) << "75s" << std::setw(10) << "90s"
+            << std::setw(10) << "105s" << std::setw(10) << "120s" << std::endl;
+
+  std::cout << std::setw(10) << "Recursiva";
+
+  for (int i = 0; i < 8; i++) {
+    std::cout << std::setw(10) << recursive_data[i];
+  }
+
+  std::cout << std::endl;
+
+  std::cout << std::setw(10) << "Iterativa";
+
+  for (int i = 0; i < 8; i++) {
+    std::cout << std::setw(10) << iterative_data[i];
+  }
+
+  std::cout << std::endl;
 
   return 0;
 }
